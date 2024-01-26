002ca5b7450a8d80b03480bf0537630dfb85fc7a
SuperSLD <37121925+SuperSLD@users.noreply.github.com>
2024-01-26 21:23:12 +0300

    first commit

---
+++ src/main/java/online/jutter/temporal/Temporal.java
@@ -0,0 +1,32 @@
+package online.jutter.temporal;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Temporal.MODID)
+public class Temporal {
+
+    static final String MODID = "temporal";
+
+    public Temporal() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        //Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    public void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+}
